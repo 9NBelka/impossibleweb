33bb0bf93b350be87d8160a51ddd81fa1f850a23
Nikita <belka.nik02@gmail.com>
2026-06-19 14:11:17 +0200

    anal from vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import RootRedirect from '../components/RootRedirect/RootRedirect';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootPage() {
   return (
     <>
       <RootRedirect defaultLang='en' />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
